8f4eab586a81bd6d88d180f86eaa398237ce2431
Matt Campbell <mecampbell25@gmail.com>
2014-03-09 18:53:15 -0500

    [66744672] AppController authorize current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authorize
+
   private
 
   def current_user
@@ -11,7 +13,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+    redirect_to new_session_path, alert: "Not authorized" if current_user.nil?
   end
-  
+
 end
